c3948fc6decca43556d5523abe0607ff3a7d2c85
Dhruv <52994515+rathod-dhruv@users.noreply.github.com>
2025-08-26 20:33:33 -0500

    Initial Commit

---
+++ InspirationApp/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  InspirationApp
+//
+//  Created by Rathod, Dhruv on 8/26/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ InspirationApp/InspirationAppApp.swift
@@ -0,0 +1,33 @@
+//
+//  InspirationAppApp.swift
+//  InspirationApp
+//
+//  Created by Rathod, Dhruv on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct InspirationAppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
